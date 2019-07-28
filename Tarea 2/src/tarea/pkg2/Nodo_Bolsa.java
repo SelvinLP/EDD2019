77792d49574e6ae9a456712325ad7de3320ef773
SelvinLP <36603066+SelvinLP@users.noreply.github.com>
2019-07-27 21:44:29 -0600

    Tarea 2
    Primer Commit casi full

---
+++ Tarea 2/src/tarea/pkg2/Nodo_Bolsa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea.pkg2;
+
+/**
+ *
+ * @author Aragon Perez
+ */
+public class Nodo_Bolsa {
+    Object x,y;
+    Nodo_Bolsa siguiente;
+    public Nodo_Bolsa(Object x1, Object x2){
+        this.x=x1;
+        this.y=x2;
+        this.siguiente=null;
+    }
+}
