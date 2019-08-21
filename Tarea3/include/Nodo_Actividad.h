935ad4c6afce40cd35d4c70fb087872cc19a4a4e
SelvinLP <36603066+SelvinLP@users.noreply.github.com>
2019-08-20 22:25:35 -0600

    Terminado
    xiste una pausa entre los menus, ingresar cualquier valor para avanzar(lo hice con el objetivo de que no se vea amontonado)

---
+++ Tarea3/include/Nodo_Actividad.h
@@ -0,0 +1,16 @@
+#ifndef NODO_ACTIVIDAD_H
+#define NODO_ACTIVIDAD_H
+
+
+class Nodo_Actividad
+{
+    public:
+        Nodo_Actividad();
+        virtual ~Nodo_Actividad();
+
+    protected:
+
+    private:
+};
+
+#endif // NODO_ACTIVIDAD_H
