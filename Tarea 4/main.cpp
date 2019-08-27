2273880ee5e9fc1b321c5371fcbc8a072c29fe81
SelvinLP <36603066+SelvinLP@users.noreply.github.com>
2019-08-27 17:59:11 -0600

    solo falta docker

--- Tarea 4/main.cpp
+++ Tarea 4/main.cpp
@@ -16,11 +16,29 @@ class Nodo_A {
 class Lista_A {
     public:
         Nodo_A* inicio=0;
+
         void mostrardia(int day){
             Nodo_A* tem=inicio;
             while(tem !=0){
                 if(tem->dia==day){
-                    cout << "Hora: "<<tem->hora <<"  Actividad:  "<<tem->actvi << endl;
+                    cout << "Hora: "<<tem->hora <<"  Actividad:  "<<tem->actvi <<"  dia:  "<<tem->dia << endl;
+                    tem=tem->abajo;
+                }else{
+                    tem=tem->siguiente;
+                }
+
+            }
+        }
+
+        void mostrartodo(){
+            Nodo_A* tem=inicio;
+            Nodo_A* tem2=0;
+            tem=tem->siguiente;
+            while(tem !=0){
+                tem2=tem;
+                while(tem2!=0){
+                    cout << "Hora: "<<tem2->hora <<"  Actividad:  "<<tem2->actvi <<"  dia:  "<<tem2->dia << endl;
+                    tem2=tem2->abajo;
                 }
                 tem=tem->siguiente;
             }
@@ -49,71 +67,51 @@ class Lista_A {
         return 0;
     }
 
-        Nodo_A* insertarOrdenadOX(Nodo_A* nuevo, Nodo_A* cabeza){
-            Nodo_A* tem=cabeza;
-            //bandera indica si se incerta antes o despues
+        Nodo_A* crearColumnaX(int valorx){
+            Nodo_A* raizColumna=inicio;
+            Nodo_A* tem=inicio;
+            Nodo_A* fin=0;
+            Nodo_A* nuevos=new Nodo_A(valorx,"COL",-1);
             bool bandera=false;
-            while(true){
-                //metodo de ordenamiento
-                //si son iguales
-                if(tem->dia==nuevo->dia){
-                    tem->hora=nuevo->hora;
-                    tem->actvi=nuevo->actvi;
-                    return tem;
-                }else{
-                    //en caso de que no sean iguales
-                    if(tem->dia > nuevo->dia){
-                        bandera=true;
-                        break;
-                    }
+            while(raizColumna!=0){
+                if(raizColumna->dia > valorx){
+                    bandera=true;
+                    tem=raizColumna;
                 }
-                //fin comprobacion de ordenamiento
-                if(tem->siguiente!=0){
-                    tem=tem->siguiente;
-                }else{
-                    //se incerta despues para eso la bandera debe ser falsa
-                    break;
+                if(raizColumna->siguiente==0){
+                    fin=raizColumna;
                 }
+                raizColumna=raizColumna->siguiente;
             }
             if(bandera){
                 //incertamos valores antes del temporal que es el encontrado
-                nuevo->siguiente=tem;
-                tem->anterior->siguiente=nuevo;
-                nuevo->anterior=tem->anterior;
-                tem->anterior=nuevo;
+                nuevos->siguiente=tem;
+                tem->anterior->siguiente=nuevos;
+                nuevos->anterior=tem->anterior;
+                tem->anterior=nuevos;
             }else{
-                tem->siguiente=nuevo;
-                nuevo->anterior=tem;
+                fin->siguiente=nuevos;
+                nuevos->anterior=fin;
             }
-            return nuevo;
-
+            return nuevos;
         }
 
-        Nodo_A* insertarOrdenadOY(Nodo_A* nuevo, Nodo_A* cabeza){
-            Nodo_A* tem=cabeza;
-            //bandera indica si se incerta antes o despues
+        Nodo_A* crearFilaY(int valory){
+            Nodo_A* raizFila=inicio;
+            Nodo_A* tem=inicio;
+            Nodo_A* fin=0;
+            Nodo_A* nuevo=new Nodo_A(-1,"FIL",valory);
             bool bandera=false;
-            while(true){
-                //metodo de ordenamiento
-                //si son iguales
-                if(tem->hora==nuevo->hora){
-                    tem->dia=nuevo->dia;
-                    tem->actvi=nuevo->actvi;
-                    return tem;
-                }else{
-                    //en caso de que no sean iguales
-                    if(tem->hora > nuevo->hora){
-                        bandera=true;
-                        break;
-                    }
-                }
-                //fin comprobacion de ordenamiento
-                if(tem->abajo!=0){
-                    tem=tem->abajo;
-                }else{
-                    //se incerta despues para eso la bandera debe ser falsa
+            while(raizFila!=0){
+                if(raizFila->hora>valory){
+                    bandera=true;
+                    tem=raizFila;
                     break;
                 }
+                if(raizFila->abajo==0){
+                   fin=raizFila;
+                }
+                raizFila=raizFila->abajo;
             }
             if(bandera){
                 //incertamos valores antes del temporal que es el encontrado
@@ -122,60 +120,107 @@ class Lista_A {
                 nuevo->arriba=tem->arriba;
                 tem->arriba=nuevo;
             }else{
-                tem->abajo=nuevo;
-                nuevo->arriba=tem;
+                fin->abajo=nuevo;
+                nuevo->arriba=fin;
             }
             return nuevo;
         }
 
-        Nodo_A* crearColumnaX(int valorx){
-            Nodo_A* raizColumna=inicio;
-            Nodo_A* columna=insertarOrdenadOX(new Nodo_A(valorx,"COL",-1),raizColumna);
-            return columna;
+        void insertarenX(Nodo_A* nuevo, Nodo_A* Cabeza){
+            bool bandera=false;
+            Nodo_A* tem=Cabeza;
+            Nodo_A* fin=0;
+            while(Cabeza!=0){
+                if(Cabeza->hora>nuevo->hora){
+                    bandera=true;
+                    tem=Cabeza;
+                    break;
+                }
+                if(Cabeza->abajo==0){
+                    fin=Cabeza;
+                }
+                Cabeza=Cabeza->abajo;
+            }
+            if(bandera){
+                //incertamos valores antes del temporal que es el encontrado
+                nuevo->abajo=tem;
+                tem->arriba->abajo=nuevo;
+                nuevo->arriba=tem->arriba;
+                tem->arriba=nuevo;
+            }else{
+                fin->abajo=nuevo;
+                nuevo->arriba=fin;
+            }
+
         }
 
-        Nodo_A* crearFilaY(int valory){
-            Nodo_A* raizFila=inicio;
-            Nodo_A* fila=insertarOrdenadOY(new Nodo_A(-1,"FILA",valory),raizFila);
-            return fila;
+        void insertarenY(Nodo_A* nuevo, Nodo_A* Cabeza){
+            bool bandera=false;
+            Nodo_A* tem=Cabeza;
+            Nodo_A* fin=0;
+            while(Cabeza!=0){
+                if(Cabeza->dia>nuevo->dia){
+                    bandera=true;
+                    tem=Cabeza;
+                    break;
+                }
+                if(Cabeza->siguiente==0){
+                    fin=Cabeza;
+                }
+                Cabeza=Cabeza->siguiente;
+            }
+            if(bandera){
+                //incertamos valores antes del temporal que es el encontrado
+                nuevo->siguiente=tem;
+                tem->anterior->siguiente=nuevo;
+                nuevo->anterior=tem->anterior;
+                tem->anterior=nuevo;
+                bandera=false;
+            }else{
+                fin->siguiente=nuevo;
+                nuevo->anterior=fin;
+            }
         }
 
         void InsertarActividad(int horaY,int diaX, string actividad){
             //crecion de nuevo nodo con los datos
             Nodo_A* nuevo=new Nodo_A(diaX,actividad,horaY);
-
             Nodo_A* columna=buscarX(diaX);
             Nodo_A* fila=buscarY(horaY);
             //casos de insercion
             //primer caso columna y fila no existe
             if(columna==0 && fila==0){
-            columna=crearColumnaX(diaX);
-            fila=crearFilaY(horaY);
+                cout << "crea c y f"<< endl;
+                columna=crearColumnaX(diaX);
+                fila=crearFilaY(horaY);
+                insertarenX(nuevo,columna);
+                insertarenY(nuevo,fila);
             }else{
-                //segundo caso de inserccion columna no exista y fila si
-                if(columna==0 && fila!=0){
-                    columna=crearColumnaX(diaX);
-                    nuevo=insertarOrdenadOX(nuevo,columna);
-                    nuevo=insertarOrdenadOY(nuevo,fila);
-
-                }else{
-                    //tercer caso de inserccion columna exosta y fila no exista
-                    if(columna!=0 && fila==0){
-                        fila=crearFilaY(horaY);
-                        nuevo=insertarOrdenadOX(nuevo,columna);
-                        nuevo=insertarOrdenadOY(nuevo,fila);
-
+                if(columna!=0 && fila==0){
+                    cout << "crea f"<< endl;
+                    //segundo caso si columna existe
+                    fila=crearFilaY(horaY);
+                    insertarenX(nuevo,columna);
+                    insertarenY(nuevo,fila);
+                }
+                else{
+                    if(columna==0 && fila!=0){
+                        cout << "crea c "<< endl;
+                        //tercer caso si fila existe
+                        columna=crearColumnaX(diaX);
+                        insertarenX(nuevo,columna);
+                        insertarenY(nuevo,fila);
                     }else{
-                        //cuarto caso de inserccion columna exista y fila exista
                         if(columna!=0 && fila!=0){
-                            nuevo=insertarOrdenadOX(nuevo,columna);
-                            nuevo=insertarOrdenadOY(nuevo,fila);
+                            cout << "no crea c y f"<< endl;
+                            //si los dos existen
+                            insertarenX(nuevo,columna);
+                            insertarenY(nuevo,fila);
                         }
                     }
                 }
             }
 
-        //fin de casis de inserccion
     }
 };
 int main()
@@ -220,7 +265,7 @@ int main()
             if(opcion2==2){
 
                 cout << "Imprimiendo actividades de la semana: "<< endl;
-
+                listap->mostrartodo();
             }
 
         }
