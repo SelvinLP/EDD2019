eb81cb6dd1e39e6c32d0285874edde18ba65760b
SelvinLP <36603066+SelvinLP@users.noreply.github.com>
2019-09-06 17:08:07 -0600

    Terminado
    Areglado lo de insertar ordenado

--- Tarea3/main.cpp
+++ Tarea3/main.cpp
@@ -1,8 +1,13 @@
+#include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 class Nodo_Actividades {
     public:
+        //Lista Doble de Actividades
         string Actividad;
         Nodo_Actividades* siguiente;
         Nodo_Actividades* anterior;
@@ -35,6 +40,7 @@ class Lista_Actividades {
             }
         }
 };
+//clse de dias, esta clase representa una lista ordenada de los dias y cada nodo es una lista de actividades
 class Nodo_A {
     public:
         int fecha;
@@ -47,10 +53,46 @@ class Lista_A {
     public:
         Nodo_A* inicio=0;
         void Insertar (int dato,string ac, Lista_Actividades* lts){
-            Nodo_A* tem=new Nodo_A(dato,ac,lts);
-            tem->listaactividades->Insertar(ac);
-            tem->siguiente=inicio;
-            inicio=tem;
+            Nodo_A* nuevo=new Nodo_A(dato,ac,lts);
+            nuevo->listaactividades->Insertar(ac);
+
+            //insercion de datos ordenados de una lista simple, se utilizo un metodo iterativo
+            //metodo complicado dado que es una lista simple :(
+            Nodo_A *tem=inicio;//para recorrer la lista
+            Nodo_A *fin;//creo un apuntador de final se me olvido hacerlo fuera xd
+            Nodo_A *cambio;//guarda un valor anterior para efectuar los enlaces despues
+            bool bandera=false;//sirve para indicar si agrego antes o al final
+            if(inicio==0){
+                inicio=nuevo;
+            }else{
+                //si el dato es menor al inicial
+               if(inicio->fecha>nuevo->fecha){
+                    nuevo->siguiente=inicio;
+                    inicio=nuevo;
+               }else{
+                    while(tem!=0){
+                        if(tem->fecha > nuevo->fecha){
+                            bandera=true;
+                            break;
+                        }
+                        if(tem->siguiente==0){
+                            fin=tem;
+                        }else{
+                            cambio=tem;
+                        }
+                        tem=tem->siguiente;
+                    }
+                    if(bandera){
+                    //inserta antes
+                      nuevo->siguiente=cambio->siguiente;
+                      cambio->siguiente=nuevo;
+                      bandera=false;
+                    }else{
+                        //inserta al final
+                        fin->siguiente=nuevo;
+                    }
+               }
+            }
         }
 
         void InsertarActividad(int d, string cad){
@@ -99,7 +141,7 @@ int main()
     int pausa=0;
     Lista_A* listap=new Lista_A();
     while(pausa==0){
-
+        system("cls");
         cout << "Tarea 3 Clase 201701133" << endl;
         cout << "1.Ingresar Actividad" << endl;
         cout << "2. Mostrar Actividad" << endl;
